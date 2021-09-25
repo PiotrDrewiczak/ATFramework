4eb4a8630d89e24fd534327686a3c5494ee7ec48
pinios <fenomen1234@o2.pl>
2021-09-25 21:30:57 +0200

    Added LoadPreviousConfigFile method

--- TesterApp/MainWindow.xaml.cs
+++ TesterApp/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ namespace TesterApp
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using System.Windows;
     using Microsoft.Win32;
 
@@ -17,6 +19,8 @@ namespace TesterApp
         private readonly List<string> fileNamePathsList;
         private readonly string configurationExtension = "xml";
         private readonly string configurationFilter = "XML Files|*.xml";
+        private readonly string configurationFile = AppDomain.CurrentDomain.BaseDirectory + @"\config.cfg";
+
         private OpenFileDialog openFileDialog;
 
         /// <summary>
@@ -27,12 +31,27 @@ namespace TesterApp
             this.InitializeComponent();
             this.fileNamePathsList = new List<string>();
             this.XmlView.ItemsSource = this.fileNamePathsList;
+            this.LoadPreviousConfigFile();
         }
 
         private void Load_Click(object sender, RoutedEventArgs e)
         {
             this.LoadXmlFiles();
             this.XmlView.Items.Refresh();
+        }
+
+        private void LoadPreviousConfigFile()
+        {
+            if (File.Exists(this.configurationFile))
+            {
+                var stream = File.ReadAllLines(this.configurationFile);
+                foreach (var item in stream)
+                {
+                    this.fileNamePathsList.Add(item);
+                }
+
+                this.XmlView.Items.Refresh();
+            }
         }
 
         private void LoadXmlFiles()
@@ -48,6 +67,7 @@ namespace TesterApp
             if (!string.IsNullOrEmpty(this.openFileDialog.FileName))
             {
                 this.fileNamePathsList.Add(this.openFileDialog.FileName);
+                File.AppendAllText(this.configurationFile, this.openFileDialog.FileName + Environment.NewLine);
             }
         }
     }
